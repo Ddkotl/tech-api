4cffc48924a0efe6093bb88293057e601249689e
dd <dd5892631@gmail.com>
2024-10-01 20:12:23 +0300

    add res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, FileModule],
 })
 export class AppModule {}
